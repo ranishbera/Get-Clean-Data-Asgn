cde53d3910490d0d92792c57aaf4099183e86e88
ranishbera <ranishbera@gmail.com>
2016-01-31 23:23:47 +0530

    Create download_data.R

---
+++ download_data.R
@@ -0,0 +1,3 @@
+setwd("C:/Users/ranish.bera/Documents/Online Courses/Data Science Specialization/Part-3-Getting-Cleaning Data/Project-Get-Clean-Data/Data")
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', destfile='Data.zip', method='curl')
+unzip('Data.zip')
